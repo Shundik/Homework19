3f5298e6610850d15c91853af4c92226c134f904
Shundik <nastasandalova176@gmail.com>
2023-04-07 19:12:00 +0300

    Homework 19

---
+++ mysite/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Som', 'Hello', '123']
+
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
